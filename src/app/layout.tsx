39dd813d3238ed22e38050e5c917a544870679a4
MathiasClari <mathiasclari@noximity.com>
2024-03-26 13:12:54 +0100

    Added Login wth Google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {Props} from "next/script";
+import Providers from "@/components/Providers";
+import AppBar from "@/components/AppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,14 +12,16 @@ export const metadata: Metadata = {
   description: "Solves the problem of scheduling meetings with multiple people",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Providers>
+            <AppBar />
+
+            {children}
+        </Providers>
+        </body>
+        </html>
+    );
 }
